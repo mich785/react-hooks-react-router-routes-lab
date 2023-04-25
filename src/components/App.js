37c7acf2c4c454de65a0bb73a528b31ad6cb0765
michelle nderi <nderimichelle@gmail.com>
2023-04-26 00:43:24 +0300

    Complete Assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Route, Switch } from "react-router-dom";
 import NavBar from "./NavBar";
 import Home from "./Home";
@@ -7,7 +7,27 @@ import Directors from "./Directors";
 import Movies from "./Movies";
 
 function App() {
-  return <div>{/*{code here}*/}</div>;
+  const [page, setPage] = useState("/");
+
+  return (
+    <div>
+      <NavBar onChangePage={setPage} />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/actors">
+          <Actors />
+        </Route>
+        <Route exact path="/directors">
+          <Directors />
+        </Route>
+        <Route exact path="/movies">
+          <Movies />
+        </Route>
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
